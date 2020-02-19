dbf217018c6e960aa24f0dd34a60d3f70f7646bd
nicolas2lee <nicolas2lee@gmail.com>
2020-02-19 16:38:41 +0100

    [terraform] update terraform for dynamodb creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,37 +2,37 @@ terraform {
   backend "s3" {
     bucket = "berich-terraform-backend-state"
     key    = "berich/terraformcloud/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-west-3"
   }
 }
 
 resource "aws_dynamodb_table" "berich-dynamodb-table-fund" {
   name           = "Fund"
-#  billing_mode   = "PROVISIONED"
+  #  billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "id"
-#  range_key      = "GameTitle"
+  #  range_key      = "GameTitle"
 
   attribute {
     name = "id"
     type = "S"
   }
 
-#  ttl {
-#    attribute_name = "TimeToExist"
-#    enabled        = false
-#  }
+  #  ttl {
+  #    attribute_name = "TimeToExist"
+  #    enabled        = false
+  #  }
 
-#  global_secondary_index {
-#    name               = "GameTitleIndex"
-#    hash_key           = "GameTitle"
-#    range_key          = "TopScore"
-#    write_capacity     = 10
-#    read_capacity      = 10
-#    projection_type    = "INCLUDE"
-#    non_key_attributes = ["UserId"]
-#  }
+  #  global_secondary_index {
+  #    name               = "GameTitleIndex"
+  #    hash_key           = "GameTitle"
+  #    range_key          = "TopScore"
+  #    write_capacity     = 10
+  #    read_capacity      = 10
+  #    projection_type    = "INCLUDE"
+  #    non_key_attributes = ["UserId"]
+  #  }
 
   tags = {
     Name        = "berich-table-fund"
